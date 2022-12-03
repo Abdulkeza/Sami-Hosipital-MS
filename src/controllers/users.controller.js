39870803341b12dc5f83f523016809ce148c1e29
AdeodatusAbdul <adeodatus.niyokwiringirwa@harakameds.com>
2022-12-03 14:19:11 +0200

    add  authentications

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,3 +1,5 @@
+import expressAsyncHandler from "express-async-handler";
+
 import User from "../models/User.js";
 import {
   handleCreate,
@@ -5,8 +7,11 @@ import {
   handleDelete,
 } from "../helpers/baseHelpers.js";
 import passwordGenerator from "../utils/generatePassword.js";
-import { findUserByEmail, } from "../helpers/userHelper.js";
+import { findUserByEmail, isCorrectPassword, generateToken} from "../helpers/userHelper.js";
 
+// @desc Register a new user
+// @route /api/v1/users/register
+// @access Public 
 const httpRegisterUser = async (req, res) => {
   const { firstName, lastName, email, phone, password, role } = req.body;
 const userExist = await findUserByEmail(email)
@@ -27,22 +32,51 @@ const userExist = await findUserByEmail(email)
   handleCreate(User, newUser, res);
 };
 
+// @desc Login a new user
+// @route /api/v1/users/login
+// @access Public 
+const httpLoginUser = expressAsyncHandler(async (req, res) => {
+
+  const userInfo = req.body
+  const foundUser = await findUserByEmail(userInfo.email)
+
+  // role: foundUser.role, we should also include this in res
+  if (await isCorrectPassword(foundUser, userInfo)) {
+      res.status(200).json({
+          _id: foundUser._id,
+          email: foundUser.email,
+          lastName: foundUser.lastName,
+          firstName: foundUser.firstName,
+          token: generateToken(foundUser)
+      })
+  } else {
+      res.status(401).json({message: 'Invalid credentials'});
+  }
+})
+
+// @desc Get all users
+// @route /api/v1/users/
 const httpGetAllUsers = async (req, res) => {
   const users = await User.find({}).select("-password").sort({ firstName: 1 });
-  return users ? res.status(200).json(users) : "Internal server error";
+   (users) ? res.status(200).json(users) : res.status(200).json({message:"Internal server error"});
 };
 
+// @desc Get a user by Id
+// @route /api/v1/users/Id
 const httpGetUser = async (req, res) => {
   const { id } = req.params;
   const user = await handleGetSingle(User, id);
 
-  return !user
-    ? res.status(404).json({ status: "Fail", message: "User Not Found!" })
+   (!user) ? res.status(404).json({ status: "Fail", message: "User Not Found!" })
     : res.status(200).json(user);
 };
 
+// @desc update a users
+// @route /api/v1/users/Id
 const httpUpdateUser = () => {};
 
+// @desc delete a user
+// @route /api/v1/users/Id
 const httpDeleteUser = async (req, res) => {
   const { id } = req.params;
   const user = await handleGetSingle(User, id);
@@ -50,7 +84,7 @@ const httpDeleteUser = async (req, res) => {
     return res.status(400).json({status: "Fail", message: "Invalid user id"})
   }
   const isDeleted = await handleDelete(User, id)
-  if(isDeleted.acknowledged) return res.status(200).json({message: `User with ${user.email} removed successfully`});
+  if(isDeleted.acknowledged)  res.status(200).json({message: `User with ${user.email} removed successfully`});
 };
 
 export {
@@ -59,4 +93,5 @@ export {
   httpGetUser,
   httpUpdateUser,
   httpDeleteUser,
+  httpLoginUser
 };
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,12 +1,20 @@
-import { Router } from 'express';
-import { httpRegisterUser, httpGetAllUsers, httpGetUser, httpUpdateUser, httpDeleteUser } from "../controllers/users.controller.js";
+import { Router } from "express";
+import {
+  httpRegisterUser,
+  httpGetAllUsers,
+  httpGetUser,
+  httpUpdateUser,
+  httpDeleteUser,
+  httpLoginUser,
+} from "../controllers/users.controller.js";
 
 const router = Router();
 
-router.post('/', httpRegisterUser);
-router.get('/', httpGetAllUsers);
-router.get('/:id', httpGetUser);
-router.patch('/:id', httpUpdateUser);
-router.delete('/:id', httpDeleteUser);
+router.post("/", httpRegisterUser);
+router.get("/", httpGetAllUsers);
+router.get("/login", httpLoginUser);
+router.get("/:id", httpGetUser);
+router.patch("/:id", httpUpdateUser);
+router.delete("/:id", httpDeleteUser);
 
 export default router;
